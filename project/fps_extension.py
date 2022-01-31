6b704a03989ef1dc13eb63640eb1a811a08f94b6
otherwolf <vpe304@gmail.com>
2022-02-01 01:00:05 +0300

    add new event "anim"

--- project/fps_extension.py
+++ project/fps_extension.py
@@ -31,6 +31,7 @@ class FPSExtension:
         self.server.add_request_handler("CONNECTED", self.spawn_me_handler)
         self.server.add_request_handler("sendTransform", self.send_transform_handler)
         self.server.add_request_handler("getTime", self.get_time_handler)
+        self.server.add_request_handler("sendAnim", self.send_anim_handler)
         # Subscribing the system handlers
         self.server.add_event_handler(consts.ServerSystemActions.USER_DISCONNECT, self.on_user_gone_handler)
         self.server.add_event_handler(consts.ServerSystemActions.USER_LOGOUT, self.on_user_gone_handler)
@@ -45,7 +46,7 @@ class FPSExtension:
     def client_instantiate_player(self, player: player.Player):
         self._client_instantiate_player(player, None)
 
-    def _client_instantiate_player(self, player: player.Player, target_user: Optional[Client]):
+    def _client_instantiate_player(self, player: player.Player, target_user: Optional[Client]) -> None:
         packet = Packet(action="spawnPlayer", identifier=player.client.identifier, payload=player.to_packet())
 
         if not target_user:
@@ -53,7 +54,7 @@ class FPSExtension:
         else:
             self.server.send_to(packet, target=target_user, protocol=UDPServer)
 
-    def spawn_me_handler(self, client, **kwargs):
+    def spawn_me_handler(self, client, **kwargs) -> None:
         new_player = self.world.add_or_respawn_player(client)
         if new_player:
             # Send this player data about all the other players
@@ -67,7 +68,7 @@ class FPSExtension:
             if player.client.identifier != user.identifier:
                 self._client_instantiate_player(player, user)
 
-    def send_transform_handler(self, data: Packet, client: Client, **kwargs):
+    def send_transform_handler(self, data: Packet, client: Client, **kwargs) -> None:
         received_transform = transform.Transform.from_packet(json.loads(data.payload))
         result = self.world.move_player(client, received_transform)
         if result:
@@ -77,11 +78,15 @@ class FPSExtension:
             packet.payload = self.world.get_player(client).to_packet()
             self.server.send_to(packet, protocol=UDPServer)
 
-    def send_transform(self, client, transform):
+    def send_transform(self, client: Client, transform: transform.Transform) -> None:
         transform.time_stamp = utils.get_time_now_in_ms()
         packet = Packet(action="transform", identifier=client.identifier, payload=transform.to_packet())
         self.server.send_to_all(packet, protocol=UDPServer)
 
-    def get_time_handler(self, client, **kwargs):
+    def get_time_handler(self, client: Client, **kwargs) -> None:
         packet = Packet(action="time", identifier=client.identifier, payload=utils.get_time_now_in_ms())
         self.server.send_to(packet, protocol=UDPServer)
+
+    def send_anim_handler(self, data: Packet, **kwargs) -> None:
+        data.action = "anim"
+        self.server.send_to_all(data, protocol=UDPServer)
